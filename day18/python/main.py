8a3ef2e677ea849c83acf6dcb83f0867a0e67a3e
jaruizes <jalb80@gmail.com>
2020-12-24 01:32:20 +0100

    Day 18 (Python) - Refactor

--- day18/python/main.py
+++ day18/python/main.py
@@ -1,7 +1,3 @@
-EXPECTED_RESULTS_PART_ONE = [71, 51, 26, 437, 12240, 13632]
-EXPECTED_RESULTS_PART_TWO = [231, 51, 46, 1445, 669060, 23340]
-
-
 def read_operations(filepath):
     operations_file = open(filepath, "r")
     operations = operations_file.read().splitlines()
@@ -10,29 +6,34 @@ def read_operations(filepath):
     return operations
 
 
-def execute_operations(operations, method):
+def execute_operations(operations, factor_precedence):
     operation_results = []
     for operation in operations:
-        operation_list = list(operation.replace(' ', ''))
-        res = method(operation_list)
-        operation_results.append(res)
+        operation_results.append(eval_operation(list(operation.replace(' ', '')), factor_precedence))
 
     return sum(operation_results)
 
 
-def eval_operation(operation):
+def eval_operation(operation, factor_precedence):
     operation_sign = None
     total = None
     while operation:
         o = operation.pop(0)
         if o == "*" or o == "+":
-            operation_sign = o
-            continue
+            if not factor_precedence:
+                operation_sign = o
+                continue
+            else:
+                if o == "+":
+                    operation_sign = o
+                    continue
+                elif o == "*":
+                    return total * eval_operation(operation, factor_precedence)
         elif o == ")":
             return total
         else:
             if o == "(":
-                subtotal = eval_operation(operation)
+                subtotal = eval_operation(operation, factor_precedence)
             else:
                 subtotal = int(o)
 
@@ -46,34 +47,11 @@ def eval_operation(operation):
     return total
 
 
-def eval_operation2(operation):
-    operation_sign = None
-    total = None
-    while operation:
-        o = operation.pop(0)
-        if o == "+":
-            operation_sign = o
-            continue
-        elif o == "*":
-            return total * eval_operation2(operation)
-        elif o == ")":
-            return total
-        else:
-            if o == "(":
-                subtotal = eval_operation2(operation)
-            else:
-                subtotal = int(o)
-            if operation_sign == "+":
-                total += subtotal
-            else:
-                total = subtotal
-
-    return total
-
-
 ## Asserts
-assert execute_operations(read_operations("../inputs/test.txt"), eval_operation) == sum(EXPECTED_RESULTS_PART_ONE)
-assert execute_operations(read_operations("../inputs/test.txt"), eval_operation2) == sum(EXPECTED_RESULTS_PART_TWO)
+EXPECTED_RESULTS_PART_ONE = [71, 51, 26, 437, 12240, 13632]
+EXPECTED_RESULTS_PART_TWO = [231, 51, 46, 1445, 669060, 23340]
+assert execute_operations(read_operations("../inputs/test.txt"), False) == sum(EXPECTED_RESULTS_PART_ONE)
+assert execute_operations(read_operations("../inputs/test.txt"), True) == sum(EXPECTED_RESULTS_PART_TWO)
 
-print("Part one: " + str(execute_operations(read_operations("../inputs/input.txt"), eval_operation)))
-print("Part two: " + str(execute_operations(read_operations("../inputs/input.txt"), eval_operation2)))
+print("Part one: " + str(execute_operations(read_operations("../inputs/input.txt"), False)))
+print("Part two: " + str(execute_operations(read_operations("../inputs/input.txt"), True)))
